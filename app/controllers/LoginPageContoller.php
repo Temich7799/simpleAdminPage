8743e0329636cff88297ecfff1a6d06836fbae68
Artem Glushenko <artemgl9999@gmail.com>
2022-11-05 23:30:06 +0200

    send mail

--- app/controllers/LoginPageContoller.php
+++ app/controllers/LoginPageContoller.php
@@ -21,16 +21,26 @@ class LoginPageContoller extends BaseController
             $user_password = $query[0];
             if ($user_password === $this->password) {
                 $this->saveSession();
+                $this->notifyLogged();
                 return true;
             } else return false;
         } else return false;
     }
+
     protected function saveSession()
     {
         if (!isset($_SESSION)) {
             session_start();
         }
-        setcookie('sid', session_id(), time() + 900);
+        setcookie('sid', session_id(), time() + 180);
         return $this->makeQueryToSQL("UPDATE `users` SET `session_id`='" . session_id() . "',`status`='online' WHERE `username` = '" . $this->username . "'");
+    }
+
+    private function notifyLogged()
+    {
+        [$email_address] = mysqli_fetch_row($this->makeQueryToSQL("SELECT `email` FROM `users` WHERE `username` = '" . $this->username . "'"));
+        if ($email_address !== null || $email_address !== false) {
+            mail($email_address, 'Your account was logged in', 'Details');
+        }
     }
 }
--- app/controllers/RegisterPageController.php
+++ app/controllers/RegisterPageController.php
@@ -12,10 +12,12 @@ class RegisterPageController extends BaseController
         ];
 
         if ($this->isEmailExist($email) == false) {
+
             if ($this->makeQueryToSQL("INSERT INTO `users` (`email`, `username`, `password`, `role`) VALUES ('" . $email . "', '" . $username . "', '" . $password . "', '" . $role . "')") == false) {
                 $result['content'] = 'An error has occurred, please try again';
             } else {
                 $result['status'] = true;
+                $this->notifyRegistered($email);
             }
         } else {
             $result['content'] = 'This email already exists';
@@ -28,5 +30,10 @@ class RegisterPageController extends BaseController
     {
         if (mysqli_fetch_row($this->makeQueryToSQL("SELECT `email` FROM `users` WHERE `email` = '" . $email . "'")) == null) return false;
         else return true;
+    }
+
+    private function notifyRegistered(string $email)
+    {
+        mail($email, 'Your account was succesfully registered!', 'Details');
     }
 }
