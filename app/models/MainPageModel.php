09b994b8de62f758b656db45d436fb9c4b072d1b
Artem Glushenko <artemgl9999@gmail.com>
2022-11-06 13:48:31 +0200

    add header menu

--- app/models/MainPageModel.php
+++ app/models/MainPageModel.php
@@ -4,16 +4,9 @@ require_once 'BaseModel.php';
 
 class MainPageModel extends BaseModel
 {
+
     public function renderPage()
     {
-        return $this->renderHTML('user_users-page.html.twig', [
-            'page_title' => 'Users',
-            'data' => $this->getUsersList()
-        ]);
-    }
-
-    protected function getUsersList()
-    {
-        return mysqli_fetch_all($this->makeQueryToSQL("SELECT `username`, `status` FROM `users` WHERE 1"));
+        return $this->renderHTML('user_main-page.html.twig', ['page_title' => 'Welcome!']);
     }
 }
--- app/models/UserPageModel.php
+++ app/models/UserPageModel.php
@@ -1,11 +1,16 @@
 <?php
 
-require_once 'BaseModel.php';
+require_once 'MainPageModel.php';
 
-class UserPageModel extends BaseModel
+class UserPageModel extends MainPageModel
 {
     public function renderPage()
     {
         return $this->renderHTML('user_users-page.html.twig', ['page_title' => 'Home']);
+    }
+
+    protected function getUsersList()
+    {
+        return mysqli_fetch_all($this->makeQueryToSQL("SELECT `username`, `status` FROM `users` WHERE 1"));
     }
 }
