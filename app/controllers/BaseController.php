f36f17672625823c6c1356a0bd52ac020747ba32
Artem Glushenko <artemgl9999@gmail.com>
2022-11-07 23:38:55 +0200

    refactor

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,9 +1,14 @@
 <?php
 class BaseController
 {
+    protected $host = 'localhost';
+    protected $username = 'root';
+    protected $password = 'root';
+    protected $db_name = 'simple_admin_page_db';
+
     protected function makeQueryToSQL(string $sql)
     {
-        $connect = mysqli_connect('localhost', 'root', 'root', 'simple_admin_page_db');
+        $connect = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
 
         if (!$connect) {
             print('Error: ' . mysqli_connect_error());
