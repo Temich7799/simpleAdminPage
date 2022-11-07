ca112fa309125bb608a15d6b3c5a020a9da3d13b
Artem Glushenko <artemgl9999@gmail.com>
2022-11-07 21:44:37 +0200

    show username on the main page

--- app/models/MainPageModel.php
+++ app/models/MainPageModel.php
@@ -11,9 +11,16 @@ class MainPageModel extends BaseModel
         $this->is_admin = $this->checkForAdmin();
     }
 
+    protected function getUserName()
+    {
+        if (session_id()) {
+            return mysqli_fetch_row($this->makeQueryToSQL("SELECT `username` FROM `users` WHERE `session_id` = '" . session_id() . "'"))[0];
+        } else return '';
+    }
+
     public function renderPage()
     {
-        if ($this->is_admin === true) return $this->renderHTML('admin_main-page.html.twig', ['session_id' => session_id(), 'page_title' => 'Welcome, Admin!']);
-        else return $this->renderHTML('user_main-page.html.twig', ['session_id' => session_id(), 'page_title' => 'Welcome, User!']);
+        if ($this->is_admin === true) return $this->renderHTML('admin_main-page.html.twig', ['session_id' => session_id(), 'page_title' => 'Welcome, ' . $this->getUserName() . '. You are Admin!']);
+        else return $this->renderHTML('user_main-page.html.twig', ['session_id' => session_id(), 'page_title' => 'Welcome, ' . $this->getUserName() . '. You just a user..']);
     }
 }
