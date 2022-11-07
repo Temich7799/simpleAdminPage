2d3e250287c854dacfa9c34ac71ff8eb142da2d8
Artem Glushenko <artemgl9999@gmail.com>
2022-11-07 20:31:51 +0200

    make the reset password logic

--- app/controllers/RegisterPageController.php
+++ app/controllers/RegisterPageController.php
@@ -32,8 +32,38 @@ class RegisterPageController extends BaseController
         else return true;
     }
 
+    public function updatePassword(string $email)
+    {
+        $password = $this->generatePassword();
+
+        if ($this->makeQueryToSQL("UPDATE `users` SET `password`='" . $password . "' WHERE `email` = '" . $email . "'")) {
+            $this->sendPassword($email, $password);
+            return $password;
+        }
+    }
+
+    private function generatePassword()
+    {
+        $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $str = '';
+        $max = mb_strlen($keyspace, '8bit') - 1;
+
+        if ($max < 1) {
+            throw new Exception('$keyspace must be at least two characters long');
+        }
+        for ($i = 0; $i < 8; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+        return $str;
+    }
+
     private function notifyRegistered(string $email)
     {
         mail($email, 'Your account was succesfully registered!', 'Details');
+    }
+
+    private function sendPassword(string $email, string $password)
+    {
+        mail($email, 'Your password has been reset', 'New password: ' . $password);
     }
 }
--- app/views/routes/reset_password-page_route.php
+++ app/views/routes/reset_password-page_route.php
@@ -1,9 +1,20 @@
 <?php
 
 require './app/models/ResetPasswordPageModel.php';
+require_once './app/controllers/RegisterPageController.php';
 
 $app->get('/reset-password', function ($request,  $response) {
     $the_reset_page = new ResetPasswordPageModel();
     $response->getBody()->write($the_reset_page->renderPage());
+    return $response;
+});
+
+$app->post('/reset-password', function ($request,  $response) {
+
+    $reset_password = new RegisterPageController();
+
+    ['email' => $email] = $request->getParsedBody();
+
+    $response->getBody()->write(json_encode(['content' => $reset_password->updatePassword($email)]));
     return $response;
 });
